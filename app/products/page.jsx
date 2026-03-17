83ac8746d9d57918f2d7fb8241d3584d8765cb82
shruti0101 <shrutiguptacu@gmail.com>
2026-03-17 17:44:48 +0530

    changes

--- app/products/page.jsx
+++ app/products/page.jsx
@@ -1,6 +1,10 @@
 import ProductClient from "./ProductClient";
 
-
+export const metadata = {
+  title: "Office Workstation Furniture Manufacturer | DK Modular Contractor",
+  description:
+    "Explore premium office workstation furniture by DK Modular Contractor. We manufacture modular office workstations, desks, cubicles, and customized office furniture solutions for modern workplaces across India.",
+};
 
 export default function AboutUsPage() {
   return <ProductClient />;
